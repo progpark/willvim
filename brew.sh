911b90ff12d1700ac48ce2f9fb8e65acbfb76168
MainPHP <master@mainphp.com>
2015-10-29 16:12:18 +0800

    添加Mac软件安装脚本

---
+++ brew.sh
@@ -0,0 +1,70 @@
+# 安装homebrew
+#ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+
+# 安装brew cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# brew 安装基础套件
+brew install wget
+brew install git
+brew install git-flow
+brew install curl
+brew install openssl
+brew install zsh
+brew install python
+brew install mysql
+
+# brew 安装开发必备软件
+brew install watch
+brew install source-highlight
+brew install dos2unix
+brew install aria2
+brew install maven
+brew install ctags
+brew install tmux
+brew install ack
+brew install autoconf
+brew install automake
+brew install axel
+brew install boost
+brew install distribute_setup.py
+brew install gdbm
+brew install libevent
+brew install libtool
+brew install lynx
+brew install mcrypt
+brew install memcached
+brew install mercurial
+brew install mongodb
+brew install netcat
+brew install nginx
+brew install ossp-uuid
+brew install pcre
+brew install phantomjs
+brew install pkg-config
+brew install readline
+brew install redis
+brew install scons
+brew install sqlite
+brew install ssh-copy-id
+brew install tomcat
+brew install tpp
+brew install tree
+brew install xar
+brew install cliclick
+
+# brew cask 安装基础软件
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install zotero # 收藏网页与文献管理软件
+brew cask install alfred
+brew cask install self-control # 避免分心的软件
+brew cask install qq
+brew cask install vlc # 视频播放器
+brew cask install keycue #
+brew cask install mamp
+brew cask install mysql-workbench
+brew cask install mplayerx
+brew cask install appcleaner
+brew cask install slate
